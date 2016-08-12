05dc8c258fab0db42d42b6188bee2802782309af
Viplove Chaudhary <chaudhary.viplove94@gmail.com>
2016-08-11 23:41:39 -0700

    thank page corrected (presentable)

--- test/src/AppBundle/Controller/FormSubmitController.php
+++ test/src/AppBundle/Controller/FormSubmitController.php
@@ -13,6 +13,6 @@ class FormSubmitController extends Controller
 	* @Route("/submit",name="submit, thank you")
 	*/
 	public function submitAction(Request $request) {
-		return new Response("<h1> Thank You </h1> Dekh liya ye page? Ab wapas <a href='/''>jao</a><br><h3>Hymn of the day </h3>Kahan se pizza ayi.. Ghatayein kaali kyon chayee");
+		return new Response("<h1> Thank You </h1>");
 	}
 }
